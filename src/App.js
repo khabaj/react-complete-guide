fa1399feee7a67d88ade364abf030083dbd80af7
Krystian Habaj (PGS Software) <khabaj@pgs-soft.com>
2018-01-11 11:33:39 +0100

    4_52 - Outputting Lists

--- src/App.js
+++ src/App.js
@@ -56,18 +56,9 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age} />
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            click={this.switchNameHandler.bind(this, 'Max!')}
-            changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
-
-          <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age} />
+          {this.state.persons.map(person => {
+            return <Person name={person.name} age={person.age} />;
+          })}
         </div>
       );
     }
@@ -79,7 +70,7 @@ class App extends Component {
         <button
           style={style}
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
-        
+
         {persons}
 
       </div>
