a1f0df61aed0631b44c67fd08c910b81f16033aa
Krystian Habaj (PGS Software) <khabaj@pgs-soft.com>
2018-01-11 10:59:31 +0100

    3_46 - Working with Inline Styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
@@ -37,23 +37,33 @@ class App extends Component {
   }
 
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working.</p>
-        <button onClick={() => this.switchNameHandler('Maximialian!!')}>Switch Name</button>
-        <Person 
-          name={this.state.persons[0].name}  
-          age={this.state.persons[0].age}/>
-        <Person 
-          name={this.state.persons[1].name} 
+        <button
+          style={style}
+          onClick={() => this.switchNameHandler('Maximialian!!')}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age} />
+        <Person
+          name={this.state.persons[1].name}
           age={this.state.persons[1].age}
           click={this.switchNameHandler.bind(this, 'Max!')}
           changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
-          
-        <Person 
-          name={this.state.persons[2].name} 
-          age={this.state.persons[2].age}/>
+
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age} />
       </div>
     );
   }
