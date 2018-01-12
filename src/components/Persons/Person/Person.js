844215c7a4fe1c454f904219c860aab38d129576
Krystian Habaj <khabaj@pgs-soft.com>
2018-01-12 13:26:45 +0100

    7_95 -  A Different Approach to HOCs

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import classes from './Person.css'
-import WithClass from '../../../hoc/WithClass'
+import withClass from '../../../hoc/withClass'
 
 class Person extends Component {
 
@@ -20,14 +20,14 @@ class Person extends Component {
     render() {
         console.log('[Person.js] Inside render()');
         return (
-            <WithClass classes={classes.App}>
+            <React.Fragment>
                 <p onClick={this.props.click}>I'm a {this.props.name} and I am {this.props.age} years old!
                 </p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} value={this.props.name} />
-            </WithClass>
+            </React.Fragment>
         )
     };
 };
 
-export default Person;
+export default withClass(Person, classes.Person);
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,7 @@ import React, { PureComponent } from 'react';
 import classes from '../containers/App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit'
-import WithClass from './../hoc/WithClass';
+import withClass from './../hoc/withClass';
 
 
 class App extends PureComponent {
@@ -94,17 +94,17 @@ class App extends PureComponent {
     }
 
     return (
-      <WithClass classes={classes.App}>
-        <button onClick={() => {this.setState({showPersons: true})}}>Show Persons</button>
+      <React.Fragment>
+        <button onClick={() => { this.setState({ showPersons: true }) }}>Show Persons</button>
         <Cockpit
           appTitle={this.props.title}
           showPersons={this.state.showPersons}
           persons={this.state.persons}
           clicked={this.togglePersonsHandler} />
         {persons}
-      </WithClass>
+      </React.Fragment>
     );
   }
 }
 
-export default App;
+export default withClass(App, classes.App);
