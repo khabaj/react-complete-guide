1ab04fdefc88063d92561df9064d0447f798301c
Krystian Habaj (PGS Software) <khabaj@pgs-soft.com>
2018-01-12 08:04:47 +0100

    7_78 - A Better Project Structure

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import classes from'./App.css';
-import Person from './Person/Person';
+import classes from'../containers/App.css';
+import Person from '../components/Persons/Person/Person';
 
 class App extends Component {
 
