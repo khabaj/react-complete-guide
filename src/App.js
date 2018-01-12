a7deab083aec8a51f66cfdb64697022efd8de90b
Krystian Habaj (PGS Software) <khabaj@pgs-soft.com>
2018-01-12 07:47:56 +0100

    Revert "6_74 - Using Error Boundaries (React 16+)"
    This reverts commit 18046813062ba626df4135b7dfa92d8cb7d78004.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import classes from './App.css';
+import classes from'./App.css';
 import Person from './Person/Person';
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
 
@@ -58,13 +57,12 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <ErrorBoundary key={person.id}>
-              <Person
-                click={() => this.deletePersonHandler(index)}
-                name={person.name}
-                age={person.age}
-                changed={(event) => this.nameChangedHandler(event, person.id)} />
-            </ErrorBoundary>;
+            return <Person
+              click={() => this.deletePersonHandler(index)}
+              name={person.name}
+              age={person.age}
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)} />;
           })}
         </div>
       );
@@ -86,7 +84,7 @@ class App extends Component {
         <h1>Hi, I'm a React App</h1>
         <p className={assignedClasses.join(' ')}>This is really working.</p>
         <button
-          className={btnClass}
+        className={btnClass}
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
 
