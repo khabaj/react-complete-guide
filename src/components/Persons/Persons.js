00c2890ebdfa2bf9c5afb93a2e1504821ab1b1d3
Krystian Habaj <khabaj@pgs-soft.com>
2018-01-12 13:59:00 +0100

    7_98 - Validating Props

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react';
 import Person from './Person/Person'
+import PropTypes from 'prop-types'
 
 class Persons extends PureComponent {
 
@@ -46,6 +47,13 @@ class Persons extends PureComponent {
                 changed={(event) => this.props.changed(event, person.id)} />;
         });
     }
+};
+
+Person.propTypes = {
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func
 };
 
 export default Persons;
